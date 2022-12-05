4b18a395964065e11760975ee7fbcd3d763be536
Daniel Xu <xudh47@gmail.com>
2022-12-04 17:11:30 -0800

    Updated Schemas: resolvers and typeDef

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,72 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { Book, User } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    // get a single user by either their id ortheir username
+    user: async (parent, { username }) => {
+      return User.findOne({
+        $or: [
+          { _id: user ? user._id : params.id },
+          { username: params.username },
+        ],
+      });
+    },
+  },
+
+  Mutation: {
+    createUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    // login a user, sign a token, and send it back (to client/src/components/LoginForm.js)
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("No user found with this email address");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    // save a book to a user's `savedBooks` field by adding it to the set (to prevent duplicates)
+    // user comes from `req.user` created in the auth middleware function
+    saveBook: async (parent, { user }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("No user found with this email address");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    // remove a book from `savedBooks`
+    deleteBook: async (parent, { bookId }) => {
+      return User.findOneAndUpdate(
+        { _id: user_id },
+        { $pull: { savedBooks: { bookId: params.bookId } } },
+        { new: true }
+      );
+    },
+  },
+};
+
+module.exports = resolvers;
