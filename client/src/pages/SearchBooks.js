9602163770d3cfd8f148f92ab5625c484ad55044
Daniel Xu <xudh47@gmail.com>
2022-12-07 18:50:08 -0800

    Updated pages

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -15,7 +15,7 @@ import { SAVE_BOOK } from "../utils/mutations";
 
 // Old codes
 import Auth from "../utils/auth";
-import { saveBook, searchGoogleBooks } from "../utils/API";
+// import { saveBook, searchGoogleBooks } from "../utils/API";
 import { saveBookIds, getSavedBookIds } from "../utils/localStorage";
 
 const SearchBooks = () => {
@@ -26,6 +26,8 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+
+  const [saveBook, { error, data }] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -42,7 +44,8 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await searchGoogleBooks(searchInput);
+      const response =
+        await fetch`https://www.googleapis.com/books/v1/volumes?q=${searchInput}`;
 
       if (!response.ok) {
         throw new Error("something went wrong!");
@@ -79,10 +82,9 @@ const SearchBooks = () => {
 
     try {
       const response = await saveBook(bookToSave, token);
-
-      if (!response.ok) {
-        throw new Error("something went wrong!");
-      }
+      const { data } = await saveBook({
+        variables: { bookData: { ...bookToSave } },
+      });
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
